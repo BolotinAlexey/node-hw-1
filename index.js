dc079c4a838db6f01084599b5c03aa0e13dacb66
Oleksii Bolotin <samkiral73@gmail.com>
2023-05-15 20:44:47 +0200

    added in index.js invokeAction()

--- index.js
+++ index.js
@@ -4,4 +4,28 @@ const {
   removeContact,
   addContact,
 } = require("./contacts.js");
-removeContact("QBEl1Qx7OxVOHp_DbIfmC").then(console.log);
+
+const argv = require("yargs").argv;
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      return await listContacts();
+
+    case "get":
+      return await getContactById(id);
+
+    case "add":
+      return await addContact(name, email, phone);
+
+    case "remove":
+      return await removeContact(id);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction({ action: "remove", id: "5b1BF6aTVKMYmsnOLs7CE" }).then(
+  console.log
+);
