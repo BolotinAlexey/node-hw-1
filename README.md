757342ac02a40ecf8037f7e78c19c1b5e619478d
Oleksii Bolotin <samkiral73@gmail.com>
2023-05-15 21:22:57 +0200

    added README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# NODE-HW-1
+
+> ### node index --action list
+>
+> #### https://ibb.co/vd9HWCp
+
+> ### node index --action get --id C9sjBfCo4UJCWjzBnOtxl
+>
+> #### https://ibb.co/Z6Cqhx6
+
+> ### node index --action add --name Oleksii --email example@ex.com --phone 123-456
+>
+> #### https://ibb.co/4TwrCGD
+
+> ### node index --action remove --id r874Zn4IGBDKgQSqeQkpa
+>
+> #### https://ibb.co/Z6Cqhx6
--- index.js
+++ index.js
@@ -10,22 +10,20 @@ const argv = require("yargs").argv;
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      return await listContacts();
+      return console.table(await listContacts());
 
     case "get":
-      return await getContactById(id);
+      return console.log(await getContactById(id));
 
     case "add":
-      return await addContact(name, email, phone);
+      return console.table(await addContact(name, email, phone));
 
     case "remove":
-      return await removeContact(id);
+      return console.log(await removeContact(id));
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
 }
 
-invokeAction({ action: "remove", id: "5b1BF6aTVKMYmsnOLs7CE" }).then(
-  console.log
-);
+invokeAction(argv);
