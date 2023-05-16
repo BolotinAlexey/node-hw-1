14755e8152758b9418c42bd60d1a9a40b1f86a66
Oleksii Bolotin <samkiral73@gmail.com>
2023-05-17 00:10:14 +0200

    added try/catch in contact.js & index.js

--- contacts.js
+++ contacts.js
@@ -8,27 +8,54 @@ const writeJsonFile = (data) =>
   fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
 
 async function listContacts() {
-  const req = await fs.readFile(contactsPath);
-  return JSON.parse(req);
+  try {
+    const req = await fs.readFile(contactsPath);
+    return JSON.parse(req);
+  } catch (error) {
+    throw new Error("File reading error: " + error.message);
+  }
 }
 
 async function getContactById(contactId) {
-  const data = await listContacts();
-  return data.find(({ id }) => id === contactId) || null;
+  try {
+    const data = await listContacts();
+    return data.find(({ id }) => id === contactId) || null;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
 
 async function removeContact(contactId) {
-  const data = await listContacts();
-  const result = data.find(({ id }) => id === contactId) || null;
-  if (result) writeJsonFile(data.filter(({ id }) => id !== result.id));
-  return result;
+  let data;
+  try {
+    data = await listContacts();
+  } catch (error) {
+    throw new Error(error.message);
+  }
+
+  try {
+    const result = data.find(({ id }) => id === contactId) || null;
+    if (result) writeJsonFile(data.filter(({ id }) => id !== result.id));
+    return result;
+  } catch (error) {
+    throw new Error("File writing error " + error);
+  }
 }
 
 async function addContact(name, email, phone) {
-  const data = await listContacts();
+  let data;
+  try {
+    data = await listContacts();
+  } catch (error) {
+    throw new Error(error);
+  }
   const newData = [...data, { name, email, phone, id: nanoid() }];
-  writeJsonFile(newData);
-  return newData;
+  try {
+    writeJsonFile(newData);
+    return newData;
+  } catch (error) {
+    throw new Error("File writing error " + error.mesage);
+  }
 }
 module.exports = {
   listContacts,
--- index.js
+++ index.js
@@ -8,21 +8,25 @@ const {
 const argv = require("yargs").argv;
 
 async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      return console.table(await listContacts());
+  try {
+    switch (action) {
+      case "list":
+        return console.table(await listContacts());
 
-    case "get":
-      return console.log(await getContactById(id));
+      case "get":
+        return console.log(await getContactById(id));
 
-    case "add":
-      return console.table(await addContact(name, email, phone));
+      case "add":
+        return console.table(await addContact(name, email, phone));
 
-    case "remove":
-      return console.log(await removeContact(id));
+      case "remove":
+        return console.log(await removeContact(id));
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  } catch (error) {
+    console.error(error.message);
   }
 }
 
